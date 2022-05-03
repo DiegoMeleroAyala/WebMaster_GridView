11a697b50a3d27f45b73844c9e14316478c522a0
FlutLab.io <>
2022-05-03 14:26:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:melero/galeriawebmaster.dart';
 
 void main() {
   runApp(WebMasterApp());
@@ -14,51 +15,5 @@ class WebMasterApp extends StatelessWidget {
           primarySwatch: Colors.orange,
         ),
         home: PaginaInicial());
-  }
-}
-
-class PaginaInicial extends StatefulWidget {
-  PaginaInicial({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<PaginaInicial> {
-  List<String> images = [
-    "assets/images.jpg",
-    "assets/360_F_125688615_jZWGBYMf2GiFAW6uzHQ3zOyeG8oPuYLU.jpg",
-    "assets/Que-es-Webmaster.jpg",
-    "assets/correo-electronico-futuro.jpg",
-    "assets/descarga.png",
-    "assets/precio-coste-servicios-webmaster_ico.png",
-    "assets/webmaster-madrid.jpg",
-    "assets/webmaster-work-300x300.jpg",
-    "assets/images.jpg",
-    "assets/360_F_125688615_jZWGBYMf2GiFAW6uzHQ3zOyeG8oPuYLU.jpg",
-    "assets/Que-es-Webmaster.jpg",
-    "assets/correo-electronico-futuro.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Tutorial GridView"),
-        ),
-        body: GridView.custom(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 12,
-          ),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
   }
 }
